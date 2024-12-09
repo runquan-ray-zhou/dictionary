95f71d1e972bb5b2cd7b5dfe21e27d31e47f1e12
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-12-08 23:27:03 -0500

    update about page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,29 @@
-import "./About.css"
+import "./About.css";
 
 export default function About() {
-    return (
-        <div className="about">
-            <p>PokéVocab by: Runquan (Ray) Zhou</p>
-        </div>
-    )
+  return (
+    <div className="About">
+      <img
+        className="About__img"
+        src="https://media.licdn.com/dms/image/v2/D4E03AQHV7_mind9zJQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1709449259555?e=1736985600&v=beta&t=Jim9DL0dySasr3wO2WLp8C1xrrJ_KKsTSJNO1QbbIo8"
+        alt="self portrait"
+      />
+      <p>PokéVocab By: Runquan (Ray) Zhou</p>
+      <div className="About__icons">
+        <a href="mailto:rzhou@pursuit.org">
+          <i className="fa-regular fa-envelope"></i>
+        </a>
+        <a href="https://github.com/runquan-ray-zhou">
+          <i className="fa-brands fa-github"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/runquanrayzhou/">
+          <i className="fa-brands fa-linkedin"></i>
+        </a>
+        <a href="https://github.com/runquan-ray-zhou/2.9-linknyc">
+          <i className="fa-solid fa-code"></i>
+        </a>
+      </div>
+      <p>All Rights Reserved 2024</p>
+    </div>
+  );
 }
